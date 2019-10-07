7fc29ef21be0eb9adf41371c0fa91058e0e88aaf
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-10-07 09:07:35 -0700

    first commit from school

---
+++ StudentList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Student {
+  char first[20];
+  char last[20];
+  int id;
+  float gpa;
+}
+
+void add();
+void print();
+void del();
+
+int main(){
+  bool quit = false;
+
+  while(quit == false){
+    
+  }  
+  
+  
+}    
+
+void add(){
+
+}  
+
+void print(){
+
+}
+
+void del(){
+
+}  
