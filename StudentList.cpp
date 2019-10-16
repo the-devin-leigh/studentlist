2cc0f65026a738823f2402e9eeeb845c12b8e2e1
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-10-16 09:02:53 -0700

    Everything compiles, just need to convert delete input to an int

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Student {
@@ -11,12 +12,14 @@ struct Student {
 };
 
 void add(vector<Student> &students);
-void print();
-void del();
+void print(vector<Student> students);
+void del(vector<Student> &students, int checkNum);
 
 int main(){
   int i = 0;
   char input;
+  char delInput[6];
+  int delID;
   bool quit = false;
   vector<Student> students;
   
@@ -26,40 +29,78 @@ int main(){
     cout << endl;
     if(tolower(input) == 'a'){
       add(students);
+      cout << endl;
+    }else if(tolower(input) == 'p'){
+      print(students);
+      cout << endl;
+    }else if(tolower(input) == 'd'){
+      cout << "Please enter the ID number of the student you'd like to delete: ";
+      cout << endl;
+      cin >> delInput;
+      cin.get();
+      del(students, delID);
+    }else if(tolower(input) == 'q'){
+      quit = true;
+    }else{
+      cout << "Please enter a valid command." << endl;
     }
-    if(tolower(input) == 'p'){
-      
-    }
-    if(tolower(input) == 'd'){
-
-    }  
-  }  
-  
+  }
   
 }    
 
 void add(vector<Student> &students){
-  Student s;
+  char first[20];
+  char last[20];
+  int idNum = 0;
+  float gpaNum = 0;
   cout << "Enter student's first name: ";
-  cin >> s.first;
+  cin >> first;
+  cin.get();
   cout << endl;
   cout << "Enter student's last name: ";
-  cin >> s.last;
+  cin >> last;
+  cin.get();
   cout << endl;
   cout << "Enter student's ID number: ";
-  cin >> s.id;
+  cin >> idNum;
+  cin.get();
   cout << endl;
   cout << "Enter student's GPA: ";
-  cin >> s.gpa;
+  cin >> gpaNum;
+  cin.get();
   cout << endl;
 
+  Student s;
+  strcpy(s.first, first);
+  strcpy(s.last, last);
+  s.id = idNum;
+  s.gpa = gpaNum;
   students.push_back(s);
 }  
-
-void print(){
-
+ 
+void print(vector<Student> students){
+  //vector<Student>::iterator ptr;
+  for(int i = 0; i < students.size(); i++){
+    cout << "Name: " << students[i].last << ", ";
+    cout << students[i].first << endl;
+    cout << "ID Number: " << students[i].id << endl;
+    cout << "GPA: " << fixed << setprecision(2) << students[i].gpa << endl;
+    cout << endl;
+  }
 }
 
-void del(){
+void del(vector<Student> &students, int checkNum){
+  int deletedCount = 0;
+  //vector<Student>::iterator ptr;
+  for(int i = 0; i < students.size(); i++){
+    if(checkNum == students[i].id){
+      cout << "Student " << students[i].id << " has been deleted." << endl;
+      students.erase(students.begin()+i);
+      deletedCount++;
+    }
+  }
 
-}  
+  if(deletedCount == 0){
+    cout << "No students matched that ID number." << endl;
+  }
+}
